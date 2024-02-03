963c9512ad813bff54139d7bf37401eca8ba6bb2
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2024-02-03 12:31:44 -0800

    added components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Team from "./components/Team";
+import Landing from "./components/landing";
+import Rov from "./components/Rov";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
- 
+     <Landing/>
+     <Team/>
+     <Rov/>
     </main>
   );
 }
